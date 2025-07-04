889b6a814d2987a72919be046915922baa0bd6e4
bbungjun <youngjun3108@gmail.com>
2025-07-04 13:26:20 +0900

    deply,appmodule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -14,6 +14,8 @@ import { PageMembers } from './users/entities/page_members.entity';
 import { Submissions } from './users/entities/submissions.entity';
 import { Templates } from './users/entities/templates.entity';
 
+
+
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
@@ -22,10 +24,11 @@ import { Templates } from './users/entities/templates.entity';
       host: process.env.DB_HOST || 'localhost',
       port: parseInt(process.env.DB_PORT) || 3306,
       username: process.env.DB_USERNAME || 'root',
-      password: process.env.DB_PASSWORD || '0000',
+      password: process.env.DB_PASSWORD || '0000', 
       database: process.env.DB_DATABASE || 'jungle',
       entities: [Users, Pages, PageMembers, Submissions, Templates],
-      synchronize: true,
+      synchronize: true, // 개발 단계에서는 true 유지
+      logging: false,
     }),
     AuthModule,
     UsersModule,
